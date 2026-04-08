748c0c367158c932c9b78c2e774a2072785359d3
Shiv0130 <159130557+Shiv0130@users.noreply.github.com>
2026-04-08 22:57:28 +0200

    Add files via upload

---
+++ Lesson 5/Employee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class Employees
+    {
+        public int ID { get; set; }
+        public string name { get; set; }
+        public string surname { get; set; }
+        public double salary { get; set; }
+
+        public Employees(int id, string Name, string Surname, double Salary)
+        {
+            name = Name;
+            surname = Surname;
+            salary = Salary;
+            ID = id;
+
+        }
+
+        public void DisplayEmployeeInfo()
+        {
+            Console.WriteLine($"Employee ID: {ID}");
+            Console.WriteLine($"Employee Name: {name}");
+            Console.WriteLine($"Employee Surname: {surname}");
+            Console.WriteLine($"Employee Salary: R{salary}");
+        }
+
+    }
+}
