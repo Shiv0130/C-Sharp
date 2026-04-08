0467ca7bbe1df509f34fbd2389d8d5ccb0199084
Shiv0130 <159130557+Shiv0130@users.noreply.github.com>
2026-04-08 22:43:36 +0200

    Add files via upload

---
+++ Arrays/Program.cs
@@ -0,0 +1,32 @@
+﻿//Exercise 1:
+//int[] temps = {22,25,19,21,28,24,26 };
+
+//foreach (int temp in temps) 
+//{
+//    Console.WriteLine(temp);
+//}
+
+//int Avg = 0;
+//for (int i = 0; i < temps.Length; i++) 
+//{
+//    Avg = temps.Sum() / temps.Length;
+//}
+//Console.WriteLine($"The average temperature is {Avg}");
+
+//Console.WriteLine($"The maximum temperature is:{temps.Max()}");
+//Console.WriteLine($"The minimum temperature is:{temps.Min()}");
+
+//int Count = 0;
+//for (int t = 0; t < temps.Length; t++) 
+//{
+//    if (temps[t]>25) 
+//    {
+//        Count++;
+//    }
+    
+//}
+//Console.WriteLine($"Only {Count} days was it above 25 degrees");
+
+//Exercise 2:
+
+
