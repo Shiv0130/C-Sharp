1db8da273f3236c7c0916c5c3852650392a80d9c
Shiv0130 <159130557+Shiv0130@users.noreply.github.com>
2026-04-08 22:59:18 +0200

    Add files via upload

---
+++ ConsoleNotes/ConsoleNotes/ConsoleNotes/Student.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleNotes
+{
+    public class Student
+    {
+        public string? StudentNumber { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Surname { get; set; }
+
+        public string? Qualification { get; set; }
+
+        public Student(string studentnumber, string name, string surname, string qualification)
+        {
+            StudentNumber = studentnumber;
+            Name = name;
+            Surname = surname;
+            Qualification = qualification;
+        }
+
+        public void DisplayStudentDetails(Student student)
+        {
+            Console.WriteLine($"Student Number: {student.StudentNumber}");
+            Console.WriteLine($"Student Name: {student.Name}");
+            Console.WriteLine($"Student Surname: {student.Surname}");
+            Console.WriteLine($"Student Qualification: {student.Qualification}");
+        }
+    }
+}
