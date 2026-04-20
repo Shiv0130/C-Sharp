b22c9c3f65521a7701b99b3feef8fc3e39c265f0
Shiv0130 <159130557+Shiv0130@users.noreply.github.com>
2026-04-20 11:34:48 +0200

    Classes with polymorphism, abstraction

---
+++ class_polymorphism/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_polymorphism
+{
+    public class Student : Person
+    {
+        //Polymorphism (override)
+        public override void DisplayRole()
+        {
+            Console.WriteLine($"I am a Student. My name is {Name}");
+        }
+    }
+}
