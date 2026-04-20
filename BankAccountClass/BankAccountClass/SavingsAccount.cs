f25dc0a4dc30ca231274c2b5f699b9a25dfc6e8f
Shiv0130 <159130557+Shiv0130@users.noreply.github.com>
2026-04-20 12:08:49 +0200

    BankAccountBalance class

---
+++ BankAccountClass/BankAccountClass/SavingsAccount.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccountClass
+{
+    public class SavingsAccount : BankAccount
+    {
+        //Polymorphism (override)
+        public override void Withdraw(double amount)
+        {
+            balance -= amount;
+        }
+    }
+}
