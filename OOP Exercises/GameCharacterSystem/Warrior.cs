51729c21a508f9806f4a0cd5309bee252bc215f8
Shiv0130 <159130557+Shiv0130@users.noreply.github.com>
2026-04-22 11:09:32 +0200

    Character class updated
    Added a constructor to initialize health for the Warrior class.

--- OOP Exercises/GameCharacterSystem/Warrior.cs
+++ OOP Exercises/GameCharacterSystem/Warrior.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +8,11 @@ namespace GameCharacterSystem
 {
     public class Warrior : Character 
     {
+        //correction - added constructor to set health for Warrior
+        public Warrior(int health) : base(health) 
+        {
+            
+        }
         public override string Attack()
         {
             return "Warrior swings a sword";
