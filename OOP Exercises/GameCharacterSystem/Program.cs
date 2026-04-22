bea834dc06eb5f24bcc13324feb3417092c6d357
Shiv0130 <159130557+Shiv0130@users.noreply.github.com>
2026-04-22 11:07:28 +0200

    Updated GameCharacterSystem main
    Updated character initialization and attack output.

--- OOP Exercises/GameCharacterSystem/Program.cs
+++ OOP Exercises/GameCharacterSystem/Program.cs
@@ -1,12 +1,24 @@
-﻿//Need to correct this program
+//My attempt code
+//using GameCharacterSystem;
+
+//Character warrior = new Warrior();
+//warrior.SetHealth();
+//warrior.TakeDamage(10);
+//Console.WriteLine(warrior.Attack());
+
+//Character mage = new Mage();
+//mage.SetHealth();
+//mage.TakeDamage(20);
+//Console.WriteLine(mage.Attack());
+
+//correction
 using GameCharacterSystem;
 
-Character warrior = new Warrior();
-warrior.SetHealth();
-warrior.TakeDamage(10);
-Console.WriteLine(warrior.Attack());
+Character warrior = new Warrior(1000); // Set initial health for the warrior
+Character mage = new Mage(1000);
 
-Character mage = new Mage();
-mage.SetHealth();
-mage.TakeDamage(20);
-Console.WriteLine(mage.Attack());
+Console.WriteLine($"{mage.Attack()}");
+Console.WriteLine($"{warrior.Attack()}");
+
+warrior.TakeDamage(68);
+mage.TakeDamage(70);
