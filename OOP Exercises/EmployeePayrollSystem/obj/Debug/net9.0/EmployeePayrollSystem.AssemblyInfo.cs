c11d0d300f5130c6d242d6b01a7009a1cb179795
DESKTOP-SI0FP23\Shivaar <shivaar0103@gmail.com>
2026-04-27 09:09:06 +0200

    Oop updated exercises

--- OOP Exercises/EmployeePayrollSystem/obj/Debug/net9.0/EmployeePayrollSystem.AssemblyInfo.cs
+++ OOP Exercises/EmployeePayrollSystem/obj/Debug/net9.0/EmployeePayrollSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeePayrollSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+679daa957c5f221379de68b2a72dc3a284a82f07")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+882e730f1459508deb8af6f072ccae958d40f9d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeePayrollSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeePayrollSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
